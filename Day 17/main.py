53dc84a10e9aeceaac1618c38efc3d270e75d710
Vitor de Meira Gomes <vitormeiragomes@outlook.com>
2025-08-06 23:01:26 -0300

    Finishing

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,9 +1,28 @@
 class User:
     id: str
     username: str
+    age: int
+    
+    def __init__(self, id: str = "", username: str = "", age: int = 0):
+        self.id = id
+        self.username = username
+        self.age = age
+
+    def birthday(self):
+        self.age += 1
+        
+    def print_info(self):
+        print(f"ID: {self.id}, Username: {self.username}, Age: {self.age}")
+        
     
     
     
-user_1 = User()
-user_1.id = "001"
-user_1.username = "Pato"
+    
+user_1 = User("001", "Pato")
+
+user_1.print_info()
+
+for i in range(10):
+    user_1.birthday()
+    
+user_1.print_info()
